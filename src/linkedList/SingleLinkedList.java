a0f1301a91b78869baa8f01415eb5baca5e45aad
glows777 <1914426389@qq.com>
2022-01-03 17:30:58 +0800

    feat(LinkedLIst):实现链表的一些基本功能，还未全部实现

--- src/linkedList/SingleLinkedList.java
+++ src/linkedList/SingleLinkedList.java
@@ -61,12 +61,13 @@ public class SingleLinkedList {
      * 表头部插入
      * 这种操作将于输入的顺序相反，逆序
      **/
-    public void insertToHead(int value) {
+    public void insertNodeToHead(int value) {
         Node newNode = new Node(value, null);
-        insertToHead(newNode);
+        insertNodeToHead(newNode);
     }
 
-    public void insertToHead(Node newNode) {
+    public void insertNodeToHead(Node newNode) {
+        // 如果链表是空的
         if (head == null) {
             head = newNode;
         } else {
@@ -75,8 +76,68 @@ public class SingleLinkedList {
         }
     }
 
+    /**
+     * 顺序插入
+     * 链表尾部插入
+     **/
+    public void insertNodeTail(int value){
+        Node newNode = new Node(value, null);
+        // 如果链表是空的
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node q = head;
+            while (q.next != null) {
+                q = q.next;
+            }
+            //  newNode.next = q.next; 这一句，q.next可替换为null 都一样
+            newNode.next = q.next;
+            q.next = newNode;
+        }
+    }
 
+    /**
+     * 在某结点后插入
+     **/
+    public void insertNodeAfter(Node p, int value) {
+        Node newNode = new Node(value, null);
+        insertNodeAfter(p, newNode);
+    }
+    public void insertNodeAfter(Node p, Node newNode) {
+        // 判断被插入的节点p是否存在
+        if (p == null) {
+            return;
+        }
 
+        newNode.next = p.next;
+        p.next = newNode;
+     }
+
+    /**
+     * 在某结点前插入
+     **/
+    public void insertNodeBefore(Node p, Node newNode) {
+      if (p == null) {
+          return;
+      }
+      // 判断是否为首结点
+      if (head == null) {
+          insertNodeToHead(newNode);
+          return;
+      }
+
+      Node q = head;
+      // 寻找p节点的前一个结点
+      while(q != null && q.next != p) {
+          q = q.next;
+      }
+      // 防止p节点不在链表中，也就是q节点是尾结点时，还未找到p结点的前一个结点
+      if (q == null) {
+          return;
+      }
+      newNode.next = p;
+      q.next = newNode;
+    }
 
 
 
